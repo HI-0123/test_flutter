bc2518f8604b0a397e08ef37a2448ae98e60f141
HI-0123 <hiroki.ito.w@gmail.com>
2022-03-23 21:50:09 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import './pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BlendModePage(),
+      home: const AuthButton(),
     );
   }
 }
--- lib/pages/auth_button.dart
+++ lib/pages/auth_button.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthButton extends StatelessWidget {
   const AuthButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            final credential = await _auth.signInAnonymously();
+            print(credential.user?.uid);
+          },
+          child: const Text("匿名"),
+        ),
+      ),
+    );
   }
 }
